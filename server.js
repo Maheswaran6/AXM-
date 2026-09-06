63995879867bd64ba80db4e68a4b4abccb7fe10c
Maheswaran6 <smaheswaran577@gmail.com>
2026-09-06 23:03:33 +0530

    Change static file serving directory to root

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ fs.mkdirSync(UPLOADS, { recursive: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 const upload = multer({
   dest: UPLOADS,
